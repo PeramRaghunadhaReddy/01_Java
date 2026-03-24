e93b1af987eb831b4ef2cd491ba4af81abcda500
PeramRaghunadhaReddy <peramraghunadhareddy@gmail.com>
2026-03-24 10:49:22 +0530

    Added Control Statements folder

---
+++ Control/conditional/loops/RightAngledTriangleFlipInverted.java
@@ -0,0 +1,61 @@
+package task.conditional.loops;
+
+public class RightAngledTriangleFlipInverted {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = n; i >= 1; i--){
+            for (int j = i; j >= 1; j--){
+                System.out.print("* ");
+            }
+            System.out.println(" ");
+        }
+        int num = 1;
+        for (int i = n; i >= 1; i--){
+            for (int j = i; j >= 1; j--){
+                System.out.print(num+" ");
+                num++;
+            }
+            System.out.println(" ");
+        }
+        char ch = 'A';
+        for (int i = n; i >= 1; i--){
+            for (int j = i; j >= 1; j--){
+                System.out.print(ch+" ");
+                ch++;
+            }
+            System.out.println(" ");
+        }
+        System.out.println("Vertically Flipped Right-Angled Triangle:");
+        for (int i = n; i >= 1; i--) {
+            for (int j = n - i; j > 0; j--) {
+                System.out.print("  ");
+            }
+            for (int j = i; j >= 1; j--) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        int num1 = 1;
+        for (int i = n; i >= 1; i--) {
+            for (int j = n - i; j > 0; j--) {
+                System.out.print("  ");
+            }
+            for (int j = i; j >= 1; j--) {
+                System.out.print(" "+num1);
+                num1++;
+            }
+            System.out.println();
+        }
+        char ch1 = 'a';
+        for (int i = n; i >= 1; i--) {
+            for (int j = n - i; j > 0; j--) {
+                System.out.print("  ");
+            }
+            for (int j = i; j >= 1; j--) {
+                System.out.print(" "+ch1);
+                ch1++;
+            }
+            System.out.println();
+        }
+    }
+}
